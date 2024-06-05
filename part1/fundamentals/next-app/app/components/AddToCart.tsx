b7580ab66997bac7eb2fb9838c2d856f1be82b1e
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-06-05 15:17:52 +0300

    add the componets

---
+++ part1/fundamentals/next-app/app/components/AddToCart.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button onClick={() => console.log("add to cart")}>Add To Cart</button>
+    </div>
+  );
+};
+
+export default AddToCart;
