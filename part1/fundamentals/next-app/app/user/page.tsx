25ec5907cfb83be54e4cb82a93f8a780d53e45b8
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-06-07 14:32:38 +0300

    add basics of routing

--- part1/fundamentals/next-app/app/user/page.tsx
+++ part1/fundamentals/next-app/app/user/page.tsx
@@ -1,39 +1,11 @@
 import React from "react";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UsersTable from "./UsersTable";
 
 const UserPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    // cache: "no-store",
-    // next: {
-    //     revalidate: 10
-    // }
-  });
-  const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleTimeString()}</p>
-      <table className="table tabs-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UsersTable />
     </>
   );
 };
