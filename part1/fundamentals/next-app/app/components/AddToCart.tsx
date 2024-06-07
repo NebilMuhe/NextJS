573febfebcd974f0f0becedaf404b39306e3b720
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-06-07 12:12:03 +0300

    add the daisyui

--- part1/fundamentals/next-app/app/components/AddToCart.tsx
+++ part1/fundamentals/next-app/app/components/AddToCart.tsx
@@ -4,7 +4,12 @@ import React from "react";
 const AddToCart = () => {
   return (
     <div>
-      <button onClick={() => console.log("add to cart")}>Add To Cart</button>
+      <button
+        className="btn btn-primary"
+        onClick={() => console.log("add to cart")}
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
